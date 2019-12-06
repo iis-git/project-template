cd2951184fd78dc31b71e82fb54a4984e2267c62
iis <mr.samusenko@icloud.com>
2019-12-06 20:42:16 +0300

    fixed ip

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,5 @@
 import '../styles/styles.css'
 import 'lazysizes'
-import MobileMenu from './modules/MobileMenu'
-import StickyHeader from './modules/StickyHeader'
-
-new StickyHeader()
-new MobileMenu()
-
 
 if (module.hot) {
     module.hot.accept()
