7a33db943b0e5b083b8c3a8e7a5228e09a74cfab
iis <mr.samusenko@icloud.com>
2019-12-06 20:39:45 +0300

    revert last fix

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,16 @@
 import '../styles/styles.css'
 import 'lazysizes'
+import MobileMenu from './modules/MobileMenu'
+import StickyHeader from './modules/StickyHeader'
+
+new StickyHeader()
+new MobileMenu()
 
 
 if (module.hot) {
     module.hot.accept()
 }
+
 
 
 
